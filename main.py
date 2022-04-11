327ce67a61632dfe637d79da572b2aec12e2dc4b
galavner <gal.avner@weizmann.ac.il>
2022-04-11 14:05:15 +0300

    DataPredictor testing, not wqorking

--- main.py
+++ main.py
@@ -16,9 +16,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.impute import KNNImputer
 from sklearn import preprocessing
 
-from LabData.DataLoaders.Loader import Loader
+# from LabData.DataLoaders.Loader import Loader
 from LabData.DataLoaders.BodyMeasuresLoader import BodyMeasuresLoader
-from LabData.DataPredictors import SinglePredictor
+from LabData.DataMergers.DataMerger import DataMerger
+from LabData.DataPredictors.DataPredictors import DataPredictor
+from LabData.DataPredictors.PredictorParams import PredictorParams
+from LabUtils.addloglevels import sethandlers
+from LabUtils.Utils import mkdirifnotexists
+from LabData.DataAnalyses.TenK_Trajectories.archive.defs import config, DATA_SETS, MAIN_PREDS_DIR, SCREEN_BASELINE
 
 #
 # def load_filter_data():
@@ -32,12 +37,36 @@ def print_hi(name):
     print('Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+
+
+def alter_categories(df: pd.DataFrame):
+    res = df[[c for c
+        in list(df)
+        if (len(df[c].unique()) - 1 > 10 or len(df[c].unique()) - 1 == 2)]]
+    res = pd.get_dummies(res, dummy_na=False, drop_first=True)
+
+    # nuniques = df.nunique(axis = 0, dropna = True)
+    # for feature in nuniques.items():
+    #     num_uniques = feature[1]
+    #     if num_uniques < 2:
+    #         df.drop(feature[0])
+    #         continue
+    #     elif num_uniques == 2:
+    #         df[feature[0]] = pd.get_dummies(df[feature[0]], dummy_na=False, drop_first=True)
+    #     elif num_uniques < 10:
+    #         df.drop(df[feature[0]], axis = 1)
+    #         continue
+    return res
+
+
+
+
 def get_relevant_patients_per_outcome(df : pd.DataFrame , y):
     # Get DF and a feature, returns the DF with only relevant patient with non-NAN entries in the feature column
     relevant_patients = df[y].notna()#consider only non-NAN patients
     number_of_patients = relevant_patients.sum() #if this is zero, we should skip this feature as y
     x = df[relevant_patients]
-    return x, number_of_patients;
+    return x, number_of_patients
 
 
 def remove_nan_columns(df : pd.DataFrame):
@@ -65,43 +94,53 @@ def impute_data(X_train, X_test):
     imputer.transform(X_test)
 
 
-def normalize (X_train, X_test):
+def normalize (X):
     scaler = preprocessing.StandardScaler()
-    scaler.fit_transform(X_train)
-    scaler.transform(X_test)
+    scaler.fit_transform(X)
+    # scaler.transform(X_test)
+
+def XY_gen_f(x_df, y_col):
+    X , Y = DataMerger(x_df, y_col).get_xy(y_col = y_col, inexact_index='Date')
+    return f"BML {y_col}", X, Y, None, None
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+def Xy_gen_fs():
     # Load only 10K cohort with only first appointment patients
-    df = BodyMeasuresLoader().get_data(study_ids='10K', groupby_reg='first').df
-    #The following is a temp solution, in future we can map non-numerics to numerics
-    df = df.select_dtypes(include = np.number)
+    df = BodyMeasuresLoader().get_data(study_ids='10K', groupby_reg='first', min_col_present = 500).df
+
+    df = alter_categories(df)
     for y in df.columns:
         df_filtered, num_of_patients = get_relevant_patients_per_outcome(df, y)
-        if num_of_patients < 500:
-            warnings.warn('Feature ' + y + ' has less than 500 patients, skipping')
-            continue
-
         df_filtered = remove_nan_columns(df_filtered)
-
-
         df_filtered = remove_outliers(df_filtered)
         # it shouldn't matter if it's done before train-test split as it randomly assigns them to each group
 
         X = df_filtered.loc[:, df_filtered.columns != y]
         Y = df_filtered.loc[:, y]
-        # X = X.values.reshape(X.shape)
-        # Y = Y.values.reshape(len(Y), 1)
-        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+
+        # X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
 
         # Before imputing the data, it might be a nice to see if they are missing at random or not
-        impute_data(X_train, X_test)
-        normalize(X_train, X_test)
+        # impute_data(X_train, X_test)
 
-        prediction = SinglePredictor.SinglePredictor()
 
-        # df_filtered = remove_nan_rows(df_filtered)
+        normalize(X)
+
+        yield lambda: XY_gen_f(X, y)
+
+
+def main():
+    # sethandlers(file_dir=config.log_dir)
+    sethandlers()
+    y_col = 'waist'
+    # analysis_dir = '/net/mraid08/export/genie/LabData/Tom'
+    analysis_dir = '/net/mraid08/export/genie/LabData/Analyses/galavner'
+    res = DataPredictor(PredictorParams('--predictor_type XGBRegressor --num_cv_folds 3')).predict_multi(Xy_gen_fs,work_dir=analysis_dir)
+    print(res)
+
+if __name__ == '__main__':
+    main()
 
 
 
